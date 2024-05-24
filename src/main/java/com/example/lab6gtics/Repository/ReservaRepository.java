f95f8b0ad3cf7b187d012539c71d2a3c287afbb1
Hineill <hineill.cespedes@pucp.edu.pe>
2024-05-23 21:10:55 -0500

    Hola

--- src/main/java/com/example/lab6gtics/Repository/ReservaRepository.java
+++ src/main/java/com/example/lab6gtics/Repository/ReservaRepository.java
@@ -2,6 +2,13 @@ package com.example.lab6gtics.Repository;
 
 import com.example.lab6gtics.Entity.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReservaRepository extends JpaRepository<Reserva, Integer> {
+    @Query(nativeQuery = true, value="select * from reserva where idUsuario = ?1 ")
+    List<Reserva> getReservaPorIdCliente(int idCliente);
+
+
 }
