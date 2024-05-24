8f8a969df1ecddd2fbb58c7c54925fee121498a2
Hineill <hineill.cespedes@pucp.edu.pe>
2024-05-23 19:31:35 -0500

    first commit

---
+++ src/main/java/com/example/lab6gtics/Lab6gticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab6gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab6gticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab6gticsApplication.class, args);
+    }
+
+}
